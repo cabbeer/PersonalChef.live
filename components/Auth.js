544f19ae778e8502a28071046cc149e8e07a99e7
Kabir Sewani <kabir.sewani@gmail.com>
2023-02-11 18:25:59 -0500

    implimented useQuery hook in useVerified hook to get realtime update of verified status

--- components/Auth.js
+++ components/Auth.js
@@ -1,31 +1,29 @@
 import useLogin from "@/hooks/useLogin";
 import useLogout from "@/hooks/useLogout";
-import useVerified from "@/hooks/useVerified";
 import pb from "@/lib/pocketbase"
 import { useForm } from "react-hook-form"
+import useVerified, {requestVerification} from "@/hooks/useVerified";
 
-import { Input } from '@chakra-ui/react'
-
+// refactored hooks with react-hooks for learning, past commits use useState
 
 export const Auth = () => {
   const logout = useLogout();
-  const { isVerified, requestVerification} = useVerified();
+  const {data: isVerified, isLoading } = useVerified();
   const {mutate: login, isLoading, isError} = useLogin();
   const {register, handleSubmit, reset} = useForm();
-
 
   // wrap this in a useEffect hook to fix error
   const isLoggedIn = pb.authStore.isValid;
 
   async function onSubmit(data) {
-    login({email: data.email, password: data.password})
+    login({email: data.email, password: data.password});
     reset();
   }
 
   if(isLoggedIn) return(
   <>
     <h1>Logged In: {pb.authStore.model.email}</h1>
-    <p>Verified: {isVerified.toString()}</p>
+    <p>Verified: {isVerified?.toString()}</p>
     {!isVerified && <button onClick={requestVerification}>Send Verification Email</button>}
     <button onClick={logout}>Log Out</button>
   </>
