51ba4ebda287bf8862e3aa3d7ee574e6ff7cb93e
Kabir Sewani <kabir.sewani@gmail.com>
2023-02-13 20:22:04 -0500

    TIME FOR SOME STYLE! - begining of UI updates + routing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,25 @@
 // NOTE: Site entry point - DO NOTE EDIT PAGE!
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClientProvider, QueryClient } from 'react-query'
 
+//Setup React-Query
+import { QueryClientProvider, QueryClient } from 'react-query'
 const queryClient = new QueryClient()
 
-export default function App({ Component, pageProps }) {
+//Components
+import Layout from "../../components/layouts/Main"
+
+
+const Website = ({ Component, pageProps, router}) => {
   return(
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <Layout router={router}>
+          <Component {...pageProps} key={router.route}/>
+        </Layout>
       </QueryClientProvider>
     </ChakraProvider>
     )
 }
+
+export default Website
