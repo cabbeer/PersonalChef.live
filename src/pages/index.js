4c2a5f0db82aecf2142d0bb1043622c7512be189
Kabir Sewani <kabir.sewani@gmail.com>
2023-02-16 12:21:25 -0500

    final changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,13 +21,20 @@ import Paragraph from "components/paragraph";
 import { BioSection, BioYear } from "components/bio";
 import { Auth } from "components/Auth";
 
+// wayy too late changes
+import { AvatarGroup } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+
+
+
 // ask mentor what this does and if i can delete it (react boilerplate)
 // const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
   return (
     <>
-      <Container>
+      <Container centerContent>
+      <Section>
         {/* <Box
           borderRadius="lg"
           bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
@@ -39,10 +46,10 @@ const Home = () => {
         </Box> */}
         <Box display={{ md: `flex` }}>
           <Box flexGrow={1}>
-            <Heading as="h2" variant="page-title">
+            <Heading as="h1" variant="page-title" align="center" size='3xl' >
               Personal Chef
             </Heading>
-            <p>Authentic dishes. Homemade. Delivered.</p>
+            <Heading size='md'>Authentic dishes. Homemade. Delivered.</Heading>
           </Box>
           {/* <Box
             flexShrink={0}
@@ -62,19 +69,47 @@ const Home = () => {
             />
           </Box> */}
         </Box>
+        </Section>
  
-        <Section delay={0.1}>
-          <Heading as="h3" variant="section-title">
+        <Section delay={0.1} centerContent>
+        <Heading as="h3" variant="section-title" align="center">
             What is PersonalChef?
           </Heading>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-          condimentum magna nec nulla porta, non maximus quam condimentum.{''}
-          <NextLink href="/works">
+
+          <BioSection align="center">
+            <Heading as="h3" size='md'>
+            It’s not just about making a profit, you see. 
+
+            </Heading>
+            
+
+          </BioSection>
+
+          </Section>
+          <Section>
+<BioSection  align="center">
+<Heading as="h3" size='lg'>
+            We're about something much bigger than that!
+
+            </Heading>
+</BioSection>
+          
+          
+          <Paragraph>We're talking about connecting people, about bringing communities together. You might call us an online marketplace, but what we really are is a place where local cooks can find their footing and create something truly special.{''}
+          {/* <NextLink href="/works">
             <Link>
               Link-Title
             </Link>
-          </NextLink>
+          </NextLink> */}
           </Paragraph>
+
+        </Section>
+
+        <Section delay={0.2} centerContent>
+          <Heading as="h3" variant="section-title">
+            Get Started Now!
+          </Heading>
+
           <Box align="center" my={4}>
           <NextLink href="/browse">
             <Button rightIcon={<ChevronRightIcon/>}>
@@ -82,13 +117,46 @@ const Home = () => {
             </Button>
           </NextLink>
           </Box>
-        </Section>
+          </Section>
 
-        <Section delay={0.2}>
-          <Heading as="h3" variant="section-title">
-            Get Started Now!
-          </Heading>
-          <BioSection>
+          <Section></Section>
+            <Heading as="h4">
+              Local shefs are waiting!
+            </Heading>
+            {/* Lorem ipsum dolor sit amet */}
+
+
+          <AvatarGroup size='md' max={5}>
+  <Avatar name='Segun Adebayo' src='https://i.imgur.com/kBRFfEk.jpg' />
+  <Avatar name='Kent Dodds' src='https://i.imgur.com/ML2oZo1.jpg' />
+  <Avatar name='Prosper Otemuyiwa' src='https://i.imgur.com/P2mW3fq.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/uVhCjKp.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+
+  <Avatar name='Christian Nwamba' src='https://i.imgur.com/6V36QD2.jpg' />
+
+
+</AvatarGroup>
+
+
+
+
+
+
+
+
+
+
+
+
+
+          {/* <BioSection>
             <BioYear>
               Step 1
             </BioYear>
@@ -99,20 +167,26 @@ const Home = () => {
               Step 2
             </BioYear>
             Lorem ipsum dolor sit amet
-          </BioSection>
-        </Section>
+          </BioSection> */}
 
-        <Section delay={0.2}>
-          <Heading as="h3" variant="section-title">
+
+        <Section delay={0.2} centerContent>
+          <Heading as="h3" variant="section-title" >
             Accessible to everyone.
           </Heading>
+          </Section>
+
           <Paragraph>
             Vegitarian, Vegan, Gluen Free,
             <Link href="https://cdn2.thecatapi.com/images/MTg3Nzk5MQ.jpg">
               Low Fodmap
             </Link>
           </Paragraph>
-        </Section>
+
+
+
+
+
 
       </Container>
     </>
